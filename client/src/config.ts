b6345b3565abc98755816dd2d24548ff9c567507
Abdul Muttaleb Maruf <ammaruf7@gmail.com>
2020-05-06 16:19:49 +0600

    jenkins

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,5 +4,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   domain: 'dev-muttalebm.auth0.com',            // Auth0 domain
   clientId: '0IY1eBpGfRS2vT9Tr3Bk0dsn2S2RDS5K',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://capstone-serverless-todo-dev.s3-website-us-east-1.amazonaws.com/callback'
 }
